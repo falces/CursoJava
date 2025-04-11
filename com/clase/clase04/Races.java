b79975e05f4c9920896eec96bf140a4cc8d080ab
Javi Rodríguez <falces@gmail.com>
2025-04-11 17:35:54 +0200

    ADD: manage exceptions

--- com/clase/clase04/Races.java
+++ com/clase/clase04/Races.java
@@ -1,5 +1,8 @@
 package com.clase.clase04;
 
+import com.clase.exceptions.NotANumber;
+import com.clase.exceptions.SelectionNotFound;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -8,32 +11,41 @@ public class Races {
     private static final ArrayList<String> teams = new ArrayList<String>();
     private static Integer numberOfRaces = 0;
     private static Integer numberOfTeams = 0;
-    private static ArrayList<Car> race = new ArrayList<>();
+    private static final ArrayList<Car> race = new ArrayList<>();
     private static final HashMap<String, Integer> results = new HashMap<>();
     private static String winner;
 
     public static void main(String[] args) {
-        addTeams();
-        askNumberOfTeams();
-        askNumberOfRaces();
-        for(int i = 1; i <= numberOfRaces; i++){
-            race.clear();
-            for(int j = 0; j < numberOfTeams; j++){
-                race.add(new Car(teams.get(j)));
-            }
-            int previousSpeed = 0;
-            for(Car car : race){
-                int currentSpeed = car.getSpeed();
-                if (currentSpeed > previousSpeed) {
-                    winner = car.getBrand();
-                    previousSpeed = car.getSpeed();
+        try {
+            addTeams();
+            askNumberOfTeams();
+            askNumberOfRaces();
+            for(int i = 1; i <= numberOfRaces; i++){
+                race.clear();
+                for(int j = 0; j < numberOfTeams; j++){
+                    race.add(new Car(teams.get(j)));
                 }
+                int previousSpeed = 0;
+                for(Car car : race){
+                    int currentSpeed = car.getSpeed();
+                    if (currentSpeed > previousSpeed) {
+                        winner = car.getBrand();
+                        previousSpeed = car.getSpeed();
+                    }
+                }
+                int currentWinnedRaces = results.getOrDefault(winner, 0);
+                results.put(winner, ++currentWinnedRaces);
             }
-            int currentWinnedRaces = results.getOrDefault(winner, 0);
-            results.put(winner, ++currentWinnedRaces);
-        }
 
-        System.out.println(results);
+            teams.forEach(
+                    (t) ->
+                            System.out.println(
+                                    t + " ha ganado " + (results.get(t) != null ? results.get(t) : 0) + " carreras"
+                            )
+            );
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     private static void addTeams(){
@@ -50,14 +62,36 @@ public class Races {
     }
 
     private static void askNumberOfTeams() {
-        System.out.println("¿Cuántos equipos quieres usar?");
-        Scanner input = new Scanner(System.in);
-        numberOfTeams = input.nextInt();
+        try {
+            System.out.println("¿Cuántos equipos quieres usar?");
+            Scanner input = new Scanner(System.in);
+            if (!input.hasNextInt()) {
+                throw new NotANumber("Valor no válido (2 - 10)");
+            }
+            numberOfTeams = input.nextInt();
+            if (numberOfTeams < 2 || numberOfTeams > 10) {
+                throw new SelectionNotFound("Opción no válida (2 - 10)");
+            }
+        } catch (NotANumber | SelectionNotFound e) {
+            System.out.println(e.getMessage());
+            askNumberOfTeams();
+        }
     }
 
     private static void askNumberOfRaces(){
-        System.out.println("¿Cuántas carreras quieres disputar?");
-        Scanner input = new Scanner(System.in);
-        numberOfRaces = input.nextInt();
+        try {
+            System.out.println("¿Cuántas carreras quieres disputar?");
+            Scanner input = new Scanner(System.in);
+            if (!input.hasNextInt()) {
+                throw new NotANumber("Valor no válido (1 - 10000)");
+            }
+            numberOfRaces = input.nextInt();
+            if (numberOfRaces < 1 || numberOfRaces > 10000) {
+                throw new SelectionNotFound("Opción no válida (1 - 10000)");
+            }
+        } catch (NotANumber | SelectionNotFound e) {
+            System.out.println(e.getMessage());
+            askNumberOfRaces();
+        }
     }
 }
